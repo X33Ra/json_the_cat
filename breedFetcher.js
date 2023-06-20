0ef233cebab62b26563afac6ed696f12c74b6a25
MrChan <mrkcwayne@gmail.com>
2023-06-20 07:51:26 -0600

    fetches things from online

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,26 @@
 const request = require('request');
 
-request('http://www.google.com', (error, response) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status Code:', response.statusCode);
-  } else {
-    console.log('Request library is loaded successfully.');
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(apiUrl, (error, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      const errorMessage = `Breed '${breedName}' not found.`;
+      callback(errorMessage, null);
+      return;
+    }
+
+    const breed = data[0];
+    const description = breed.description;
+    callback(null, description);
+  });
+};
+
+module.exports = fetchBreedDescription;
+
